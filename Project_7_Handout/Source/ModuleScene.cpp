5816f0403483c65c71db86c84990117784654696
JandroK <61042882+JandroK@users.noreply.github.com>
2020-04-21 21:40:07 +0200

    First Move Pattron

--- Project_7_Handout/Source/ModuleScene.cpp
+++ Project_7_Handout/Source/ModuleScene.cpp
@@ -36,11 +36,11 @@ bool ModuleScene::Start()
 	
 	// Enemies ---	
 	//App->enemies->spawningEnemies(4, ENEMY_TYPE::REDSHIP, 600, 130, 100, 0);
-	App->enemies->spawningEnemies(4, ENEMY_TYPE::REDSHIP, 600, 380, 50, 1);
-	App->enemies->spawningEnemies(4,ENEMY_TYPE::BROWNSHIP, 600, 180, 100, 0);
-	//App->enemies->spawningEnemies(4, ENEMY_TYPE::GREENSHIP, 1200, 300, 90, 0);
-	//App->enemies->spawningEnemies(4, ENEMY_TYPE::YELLOWSHIP, 1200, 150, 90, 0);
-	//App->enemies->spawningEnemies(2, ENEMY_TYPE::BLUESHIP, 1200, 350, 90, 0);
+	App->enemies->spawningEnemies(4, ENEMY_TYPE::REDSHIP, 800, 380, 50, 1);
+	App->enemies->spawningEnemies(4,ENEMY_TYPE::BROWNSHIP, 600, 180, 30, 0);
+	App->enemies->spawningEnemies(4, ENEMY_TYPE::GREENSHIP, 1200, 300, 90, 0);
+	App->enemies->spawningEnemies(4, ENEMY_TYPE::YELLOWSHIP, 1200, 150, 90, 0);
+	App->enemies->spawningEnemies(2, ENEMY_TYPE::BLUESHIP, 1200, 350, 90, 0);
 	
 	return ret;
 }
